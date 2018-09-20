3ab10af1fc236ac276c58b3ead7a833760bbbbb4
bradh43 <brad.hodkinson2@gmail.com>
2018-09-19 19:22:27 -0500

    Initial Commit

---
+++ lab2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  lab2
+//
+//  Created by Brad Hodkinson on 9/19/18.
+//  Copyright © 2018 Brad Hodkinson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
